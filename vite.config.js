a26fc727b34a8765c9f6a7e79c07697b7311047f
altanlabs <interfaces@altan.ai>
2025-02-09 19:38:58 +0000

    New commit

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import * as path from 'path';
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    build: {
-        rollupOptions: {
-            onwarn: function (warning, warn) {
-                if (warning.code === 'ESLINT_ERROR')
-                    return;
-                warn(warning);
-            }
-        }
-    }
+  },
 });
